034b629018fa7e8d05d9911261dd97fa1a768b10
Rene8888 <rene.hollander@hotmail.de>
2014-03-10 17:13:58 +0100

    fixed break bug

--- src/at/rene8888/schooltoolforwebuntis/data/webuntis/TimeGrid.java
+++ src/at/rene8888/schooltoolforwebuntis/data/webuntis/TimeGrid.java
@@ -2,7 +2,6 @@ package at.rene8888.schooltoolforwebuntis.data.webuntis;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,7 +29,7 @@ public class TimeGrid {
 	}
 
 	private void fillList() throws JSONException, IOException {
-		this.unitList = new ArrayList<Unit>();
+		ArrayList<Unit> rawUnits = new ArrayList<Unit>();
 		JSONArray arr = (JSONArray) ApplicationClass.getApplication().getWebUntisRequests().getData("getTimegridUnits", null);
 		// TODO get right day
 		JSONObject currjson = arr.getJSONObject(0);
@@ -40,28 +39,28 @@ public class TimeGrid {
 			int startTime = unit.getInt("startTime");
 			int endTime = unit.getInt("endTime");
 			Unit u = new Unit(Util.createTime(startTime + ""), Util.createTime(endTime + ""), UnitTag.LESSON);
-			unitList.add(u);
+			rawUnits.add(u);
 		}
 
-		List<Unit> newUnitList = new ArrayList<Unit>(unitList.size());
-
-		for (int i = 0; i < unitList.size(); i++) {
+		this.unitList = new ArrayList<Unit>();
+		
+		for (int i = 0; i < rawUnits.size(); i++) {
 			if (i != 0) {
-				Unit prev = unitList.get(i - 1);
-				Unit curr = unitList.get(i);
+				Unit prev = rawUnits.get(i - 1);
+				Unit curr = rawUnits.get(i);
 				if (prev.getEnd().equals(curr.getStart()) == false) {
 					Unit pause = new Unit(prev.getEnd(), curr.getStart(), UnitTag.BREAK);
-					newUnitList.add(prev);
-					newUnitList.add(pause);
+					this.unitList.add(prev);
+					this.unitList.add(pause);
 				} else if (prev.getEnd().equals(curr.getStart())) {
-					newUnitList.add(prev);
+					this.unitList.add(prev);
 				}
 			}
-			if (i == (unitList.size() - 1)) {
-				newUnitList.add(unitList.get(i));
+			if (i == (rawUnits.size() - 1)) {
+				this.unitList.add(rawUnits.get(i));
 			}
 		}
-		
+
 	}
 
 }
